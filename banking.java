eecf02f1bb20e086c107dcac565ec5d56b399b9e
OBigVee <adedejivictor98@gmail.com>
2020-08-10 20:00:11 +0100

    feat: data encapsulated

--- banking.java
+++ banking.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 import java.lang.Math.*;
-// public class  ScannerTest {
-//     public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
-//         System.out.println("enter a value");
-//         int value = scanner.nextInt();
-//         System.out.println("value is\t"+Integer.toString(value));
-//     }   
-// }
+
 
 public class BankingApp{
     public static void main(String[] args) {
-        BankAccount customer1 = new BankAccount("juliet","83");
-    
-        // customer1.bal = 50;
-        // customer1.previousTransactions = 40;
-        // customer1.accountName = "customer1iet";
-        // customer1.accountId = "0";
-        // customer1.deposit(90);
-        customer1.bankMenu();
+        // BankAccount customer1 = new BankAccount("juliet","83");
+    BankAccount cust = new BankAccount();
+    //BankAccount custId = new BankAccount();
+    cust.setAccountName("juliet");
+    cust.setAccountId("83");
+    String customerName = cust.getAccountName();
+    String customerId = cust.getAccountId();
+    cust.bankMenu();
+
+        /*customer1.bal = 50;
+        customer1.previousTransactions = 40;
+        customer1.accountName = "customer1iet";
+        customer1.accountId = "0";
+        customer1.deposit(90);
+        customer1.bankMenu();*/
 
     }
 }
@@ -27,19 +27,32 @@ class BankAccount{
     //instance variables
     int bal ;
     int previousTransactions;
-    String accountName;
-    String accountId;
-    
-    BankAccount(String Name,String idx){
-        accountName = Name;
-        accountId = idx;
+    private String accountName;
+    private String accountId;
+
+
+    void setAccountName(String x){
+        this.accountName = x;
     }
-    
+
+    String getAccountName(){
+         return accountName;
+    }
+
+    void setAccountId(String id){
+        this.accountId = id;
+    }
+
+    String getAccountId(){
+        return accountId;
+    }
+
     void deposit(int amount){
         if (amount != 0){
             bal = bal+amount;
             previousTransactions = amount;
-            System.out.println("you account as just be creditted wit $"+Integer.toString(amount));
+            System.out.println("you account has just be creditted wit $"+Integer.toString(amount));
+            System.out.println("$"+Integer.toString(amount)+"\tDeposited");
         }else{
             System.out.println("you can\'t deposit $"+Integer.toString(amount));
         }
@@ -52,7 +65,7 @@ class BankAccount{
         }else{
             bal = bal - amount;
             previousTransactions = -amount;
-            System.out.println("Debit Alert!\n your account has just been debitted of $"+Integer.toString(amount));
+            System.out.println("Debit Alert!!!\nyour account has just been debitted of $"+Integer.toString(amount));
             
 
         }
@@ -62,7 +75,7 @@ class BankAccount{
         if (previousTransactions > 0){
             System.out.println("deposited: "+previousTransactions);
         }else if(previousTransactions < 0){
-            System.out.println("withdrawn: "+ Math.abs(previousTransactions));
+            System.out.println("withdrawn: $"+ Math.abs(previousTransactions));
         }else{
                 System.out.println("No Transaction occured!");
             }
@@ -72,13 +85,14 @@ class BankAccount{
     void bankMenu(){
         String selectOption;
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("**************************");
         System.out.println("welcome "+ accountName);
         System.out.println("your Id is "+ accountId);
-        System.out.println("\n");
-        System.out.println("the following option performs corresponding tasks\n ");
-        System.out.println("A : Check balance \n B : Deposit \n C : withdraw \n D : check Previous transactions \n E : Exit");
-
+        System.out.println("**************************");
+        // System.out.println("\n");
+        System.out.println("The following option performs corresponding tasks\n ");
+        System.out.println(" A :| Check balance \n B :| Deposit \n C :| withdraw \n D :| check Previous transactions \n E :| Exit");
+        System.out.println("- -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -   -  -  -  -  -");
         do{
             System.out.println("=================================================================================");
             System.out.println("select an option to perform an action");
@@ -89,7 +103,7 @@ class BankAccount{
             switch (selectOption) {
                 case "A":
                     System.out.println("-------------------- ----");
-                    System.out.println("Balance:: "+ bal);
+                    System.out.println("Balance:: $"+ bal);
                     System.out.println("-------------------------");
                     break;
 
@@ -97,9 +111,12 @@ class BankAccount{
                     System.out.println("-------------------------");
                     System.out.println("Enter the amount to deposit");
                     System.out.println("-------------------------");
-                    int amount = scanner.nextInt();
+                    try {
+                        int amount = scanner.nextInt();
                     deposit(amount);
-                    System.out.println("#"+Integer.toString(amount)+"\tDeposited");
+                    } catch (Exception e) {
+                       System.out.println("enter numeric values not letters");
+                    }
                     System.out.println("------------------------------");
                     break;
                 
@@ -109,7 +126,7 @@ class BankAccount{
                     System.out.println("----------------------------");
                     int amount2 = scanner.nextInt();
                     withdraw(amount2);
-                    System.out.println("your accounted has been debitted of\t"+Integer.toString(amount2));
+                    // System.out.println("your accounted has been debitted of\t"+Integer.toString(amount2));
                     System.out.println("------------------------------------------------------------------");
                     System.out.println("\n");
                     break;
@@ -121,12 +138,13 @@ class BankAccount{
                     break;
                 
                 case "E":
-                System.out.println("-------------------------");
-                // System.out.println("Exiting App in a secs");
-                break;    
-            default:
-                System.out.println("Invaid option please select again");
-                break;
+                    System.out.println("-------------------------");
+                    System.out.println("Exiting App in a secs");
+                    break;
+
+                default:
+                    System.out.println("Invaid option please select again");
+                    break;
             }
             
             
