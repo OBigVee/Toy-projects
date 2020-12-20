4e6b89cdbf6334a74b2b9c10779bffbabb1f45a4
OBigVee <adedejivictor98@gmail.com>
2020-12-20 22:14:39 +0100

    custom list view

---
+++ CustomListView/app/src/main/java/com/example/customlistview/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.customlistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvProducts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvProducts = (ListView) findViewById(R.id.lvProducts);
+        
+
+    }
+}
