53c62e2df61ce5ba84f4ad19c6b55490470e8096
obigvee <adedejivictor98@gmail.com>
2021-01-07 09:32:35 +0100

    issue: play button not working

--- MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -2,25 +2,61 @@ package com.example.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView ivPlay;
-    String audioLink = "";
+    String audioLink = "https://dl.dropbox.com/s/8cyfl0nbdmh4upj/Lawrence%20Oyor%20if%20I%20press%20and%20press.mp3?dl=0";
+    boolean musicPlaying = false;
+    Intent serviceIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ivPlay = (ImageView) findViewById(R.id.ivPlay);
+        ivPlay.setBackgroundResource(R.mipmap.play);
+        serviceIntent = new Intent(this,MyPlayService.class);
 
         ivPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(!musicPlaying)
+                {
+                    playAudio();
+                    ivPlay.setImageResource(R.mipmap.pause);
+                    musicPlaying = true;
+                }
+                else {
+                    stopPlayService();
+                    ivPlay.setImageResource(R.mipmap.play);
+                    musicPlaying = false;
+                }
 
             }
         });
     }
+
+    private void stopPlayService() {
+        try{
+            stopService(serviceIntent);
+        }catch (SecurityException e){
+            Toast.makeText(this, "Error!\t"+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void playAudio() {
+        serviceIntent.putExtra("audioLink",audioLink);
+        try{
+            startService(serviceIntent);
+        }
+        catch (SecurityException e){
+            Toast.makeText(this, "Error!\t"+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
--- MusicPlayer/app/src/main/java/com/example/musicplayer/MyPlayService.java
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/MyPlayService.java
@@ -4,10 +4,14 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.widget.Toast;
 
 public class MyPlayService extends Service implements MediaPlayer.OnCompletionListener,MediaPlayer.OnPreparedListener,
                 MediaPlayer.OnErrorListener,MediaPlayer.OnSeekCompleteListener,MediaPlayer.OnInfoListener,
                 MediaPlayer.OnBufferingUpdateListener{
+
+    private MediaPlayer mediaPlayer;
+    String link;
     public MyPlayService() {
     }
 
@@ -20,16 +24,47 @@ public class MyPlayService extends Service implements MediaPlayer.OnCompletionLi
     @Override
     public void onCreate() {
         super.onCreate();
+
+        mediaPlayer.setOnCompletionListener(this);
+        mediaPlayer.setOnPreparedListener(this);
+        mediaPlayer.setOnErrorListener(this);
+        mediaPlayer.setOnSeekCompleteListener(this);
+        mediaPlayer.setOnInfoListener(this);
+        mediaPlayer.setOnBufferingUpdateListener(this);
+
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+       // return super.onStartCommand(intent, flags, startId);
+
+        link = intent.getStringExtra("audioLink");
+
+        mediaPlayer.reset();
+        if(!mediaPlayer.isPlaying()){
+            try{
+                mediaPlayer.setDataSource(link);
+                mediaPlayer.prepareAsync();
+            }
+            catch (Exception e){
+                Toast.makeText(this, "Error"+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        if(mediaPlayer != null )// means active
+            {
+                if(mediaPlayer.isPlaying())
+                {
+                    mediaPlayer.stop();
+                }
+
+                mediaPlayer.release();
+        }
     }
 
     @Override
@@ -39,11 +74,28 @@ public class MyPlayService extends Service implements MediaPlayer.OnCompletionLi
 
     @Override
     public void onCompletion(MediaPlayer mp) {
-
+        if(mp.isPlaying()){
+            mp.stop();
+        }
+        stopSelf();
     }
 
     @Override
     public boolean onError(MediaPlayer mp, int what, int extra) {
+        switch (what){
+            case MediaPlayer.MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK:
+                Toast.makeText(this, "MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK", Toast.LENGTH_SHORT).show();
+                break;
+            case MediaPlayer.MEDIA_ERROR_SERVER_DIED:
+                Toast.makeText(this, "MEDIA_ERROR_SERVER_DIED", Toast.LENGTH_SHORT).show();
+                break;
+            case MediaPlayer.MEDIA_ERROR_UNKNOWN:
+                Toast.makeText(this, "MEDIA_ERROR_UNKNOWN", Toast.LENGTH_SHORT).show();
+                break;
+            case MediaPlayer.MEDIA_ERROR_TIMED_OUT:
+                Toast.makeText(this, "MEDIA_ERROR_TIMED_OUT",Toast.LENGTH_SHORT).show();
+                break;
+        }
         return false;
     }
 
@@ -54,6 +106,9 @@ public class MyPlayService extends Service implements MediaPlayer.OnCompletionLi
 
     @Override
     public void onPrepared(MediaPlayer mp) {
+        if(!mp.isPlaying()){
+            mp.start();
+        }
 
     }
 
