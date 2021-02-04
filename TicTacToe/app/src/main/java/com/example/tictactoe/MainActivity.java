7fc50b1d919806f8236f874eabb3a13f99b7acb1
obigvee <adedejivictor98@gmail.com>
2021-02-04 22:06:57 +0100

    saved and share state instance across screen rotation

--- TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -160,6 +160,25 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         resetBoard();
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("roundCount",roundCount);
+        outState.putInt("player1Points",player1Points);
+        outState.putInt("player2Points",player2Points);
+        outState.putBoolean("player1Turn",player1Turn);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        roundCount = savedInstanceState.getInt("roundCount");
+        player1Points = savedInstanceState.getInt("player1Points");
+        player2Points = savedInstanceState.getInt("player2Points");
+        player1Turn = savedInstanceState.getBoolean("player1Turn");
+        //player1Turn = savedInstanceState.getInt("player1Turn");
+    }
+
 }
 
 
